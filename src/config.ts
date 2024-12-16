0dfdf9e9c5206134bfce79c882c8dcaefcaf4c2a
k.eii <68538183+jonscafe@users.noreply.github.com>
2024-12-16 15:39:17 +0000

    update profile bio to reflect current name

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/avatar.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'k.eii',
-  bio: "I'm Jonathan S. Marbun, a CTF player in the Digital Forensics field.",
+  bio: "Jonathan Sebastian Marbun",
   links: [
     {
       name: 'GitHub',
