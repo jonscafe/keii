3021077244db0ce3b26fc1aa1df3fc5dfb067e2e
k.eii <68538183+jonscafe@users.noreply.github.com>
2024-12-17 03:15:13 +0000

    update site configuration to change subtitle and enable banner

--- src/config.ts
+++ src/config.ts
@@ -8,14 +8,14 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'k.eii',
-  subtitle: 'k.eii profile',
+  subtitle: 'blog',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
-    enable: false,
+    enable: true,
     src: 'assets/images/banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/avatar.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'k.eii',
-  bio: "Jonathan Sebastian Marbun",
+  bio: "DFIR CTF Player",
   links: [
     {
       name: 'GitHub',
